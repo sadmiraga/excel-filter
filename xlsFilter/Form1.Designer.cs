766c762b9fa1871b290c80a15f9c97d5fc184b89
sadmiraga <sadmirvela@gmail.com>
2021-05-12 16:03:35 +0200

    finished

--- xlsFilter/Form1.Designer.cs
+++ xlsFilter/Form1.Designer.cs
@@ -30,31 +30,76 @@
         {
             this.excelButton = new System.Windows.Forms.Button();
             this.excelLabel = new System.Windows.Forms.Label();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.fileNameInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // excelButton
             // 
-            this.excelButton.Location = new System.Drawing.Point(342, 32);
+            this.excelButton.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.excelButton.BackgroundImage = global::xlsFilter.Properties.Resources.excel_icon;
+            this.excelButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.excelButton.Location = new System.Drawing.Point(50, 43);
             this.excelButton.Name = "excelButton";
-            this.excelButton.Size = new System.Drawing.Size(82, 78);
+            this.excelButton.Size = new System.Drawing.Size(116, 75);
             this.excelButton.TabIndex = 0;
-            this.excelButton.Text = "select excel file";
-            this.excelButton.UseVisualStyleBackColor = true;
+            this.excelButton.UseVisualStyleBackColor = false;
             this.excelButton.Click += new System.EventHandler(this.excelButton_Click);
             // 
             // excelLabel
             // 
             this.excelLabel.AutoSize = true;
-            this.excelLabel.Location = new System.Drawing.Point(339, 146);
+            this.excelLabel.Location = new System.Drawing.Point(58, 135);
             this.excelLabel.Name = "excelLabel";
             this.excelLabel.Size = new System.Drawing.Size(0, 17);
             this.excelLabel.TabIndex = 1;
+            // 
+            // generateButton
+            // 
+            this.generateButton.Location = new System.Drawing.Point(50, 205);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(459, 66);
+            this.generateButton.TabIndex = 2;
+            this.generateButton.Text = "Generate filtered excel";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // fileNameInput
+            // 
+            this.fileNameInput.Location = new System.Drawing.Point(290, 86);
+            this.fileNameInput.Name = "fileNameInput";
+            this.fileNameInput.Size = new System.Drawing.Size(219, 22);
+            this.fileNameInput.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Choose Excel File";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(291, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(218, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Enter Name for Filtered Excel FIle";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(867, 515);
+            this.ClientSize = new System.Drawing.Size(540, 299);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fileNameInput);
+            this.Controls.Add(this.generateButton);
             this.Controls.Add(this.excelLabel);
             this.Controls.Add(this.excelButton);
             this.Name = "Form1";
@@ -68,6 +113,10 @@
 
         private System.Windows.Forms.Button excelButton;
         private System.Windows.Forms.Label excelLabel;
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.TextBox fileNameInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- xlsFilter/Form1.cs
+++ xlsFilter/Form1.cs
@@ -1,27 +1,181 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections.Generic;
+//custom using 
+using Aspose.Cells;
+using MSExcel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+//using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Interop.Excel;
+
 
 //destinatario |  provincia + spedizione 
 
 namespace xlsFilter
 {
+
+    
+
     public partial class Form1 : Form
     {
+
+        //function for editing raw cell value to ID
+        public string cutID(string uneditedID)
+        {
+            //remove name
+            uneditedID = uneditedID.Substring(uneditedID.LastIndexOf("#") + 1);
+
+            //remove chars
+            char variableCheck = uneditedID[uneditedID.Length - 1];
+
+            while (!Char.IsNumber(variableCheck))
+                {
+                    uneditedID = uneditedID.Substring(0, uneditedID.Length - 1);
+                    variableCheck = uneditedID[uneditedID.Length - 1];
+
+                }
+
+            return uneditedID;
+        }
+
+        public string excelPath = "";
+        public string savePath = "";
+        public string backSlash =  @"\";
+        public string newExcelName = "";
+
+        //excel data 
+        List<string> ids = new List<string>();
+        List<string> trackingNumbers = new List<string>();
+
+
+        private MSExcel._Application excel;
+        private MSExcel._Workbook workbook;
+        private MSExcel._Worksheet worksheet;
+        private MSExcel.Sheets sheet;
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        //browse for excel file
         private void excelButton_Click(object sender, EventArgs e)
         {
+            OpenFileDialog choofdlog = new OpenFileDialog();
+            choofdlog.Filter = "Excel Worksheets 2003(*.xls)|*.xls|Excel Worksheets 2007(*.xlsx)|*.xlsx|Word Documents(*.doc)|*.doc";
+            choofdlog.FilterIndex = 1;
+            choofdlog.Multiselect = false;
 
+            if (choofdlog.ShowDialog() == DialogResult.OK)
+            {
+                string sFileName = choofdlog.FileName;
+                excelPath = sFileName;
+                excelLabel.Text = excelPath;
+            }
         }
+
+        //generate filtered excel file 
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            if(excelPath != "") { 
+
+            Excel.Application existingApp = new Excel.Application();
+            Excel.Workbook xlWorkbook1 = existingApp.Workbooks.Open(excelPath);
+            Excel._Worksheet xlWorksheet1 = xlWorkbook1.Sheets[1];
+            Excel.Range xlRange = xlWorksheet1.UsedRange;
+
+            int rowCount = xlRange.Rows.Count; //redovi - prema dole, 
+            int colCount = xlRange.Columns.Count; // kolone - u stranu
+
+            for (int i = 1; i <= rowCount; i++)
+            {
+                for (int j = 1; j <= colCount; j++)
+                {
+                    //store tracking number
+                    if (j == 1 && i!=1)
+                    {
+                        string fullTrackingNumber = "GO" + xlRange.Cells[i, j].Value2.ToString();
+                        trackingNumbers.Add(fullTrackingNumber);
+                    }
+
+                    //store ID
+                    if(j==11 && i != 1)
+                    {
+                        //get value from excel
+                        string fullid = xlRange.Cells[i, j].Value2.ToString();                        
+                        ids.Add(cutID(fullid));
+                    } // end of excel access ID IF
+                }
+             }
+
+
+            //choose folder where to save excel file
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    savePath = fbd.SelectedPath;
+                }
+            }
+
+            //generate full path for excel file
+            if(fileNameInput.Text == "")
+                {
+                    newExcelName = "sadmir";
+                } else
+                {
+                    newExcelName = fileNameInput.Text;
+                }
+
+            string fullPath = savePath + backSlash + newExcelName + ".xlsx";
+
+            //create app
+            Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+            if (xlApp == null)
+            {
+                MessageBox.Show("Excel is not properly installed!!");
+                return;
+            }
+
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+            object misValue = System.Reflection.Missing.Value;
+
+            xlWorkBook = xlApp.Workbooks.Add(misValue);
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            xlWorkSheet.Cells[1, 1] = "ID";
+            xlWorkSheet.Cells[1, 2] = "Tracking Number";
+           
+            for(int i = 0; i <= ids.Count-1; i++)
+            {
+                xlWorkSheet.Cells[i+2, 1] = ids[i];
+                xlWorkSheet.Cells[i+2, 2] = trackingNumbers[i];
+            }
+
+            xlWorkBook.SaveAs(fullPath, 51);
+            xlWorkBook.Close(true, misValue, misValue);
+            xlApp.Quit();
+
+            Marshal.ReleaseComObject(xlWorkSheet);
+            Marshal.ReleaseComObject(xlWorkBook);
+            Marshal.ReleaseComObject(xlApp);
+
+            MessageBox.Show("Filtered excel file created, you can find it in \n "+fullPath);
+
+            //clear data for next one
+            excelPath = "";
+            savePath = "";
+            newExcelName = "";
+            excelLabel.Text = "";
+
+            } else
+            {
+                MessageBox.Show("Choose excel file first");
+            }
+
+        } // end of generate button function 
     }
 }
